3d277b13b338b28ab2266770677cd84fcb81017c
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-02-15 21:48:50 +0900

    change format

--- R/thanatos.R
+++ R/thanatos.R
@@ -1,15 +1,15 @@
-thanatos_dark <- generate_named_colors(background = "#3a5068",
-                                       line = "#f06989",
-                                       text = "#f06989",
-                                       red = "#e68ca3",
-                                       blue = "#448ea9",
-                                       green = "#48CBAC",
-                                       yellow = "#EE9775",
-                                       magenta = "#BE91EB",
-                                       brightred = "#e8a7c2",
-                                       brightblue = "#77a4b9",
-                                       brightgreen = "#93E1CD",
-                                       brightyellow = "#F2C3AE",
+thanatos_dark <- generate_named_colors(background    = "#3a5068",
+                                       line          = "#f06989",
+                                       text          = "#f06989",
+                                       red           = "#e68ca3",
+                                       blue          = "#448ea9",
+                                       green         = "#48CBAC",
+                                       yellow        = "#EE9775",
+                                       magenta       = "#BE91EB",
+                                       brightred     = "#e8a7c2",
+                                       brightblue    = "#77a4b9",
+                                       brightgreen   = "#93E1CD",
+                                       brightyellow  = "#F2C3AE",
                                        brightmagenta = "#D2BCF0"
 )
 
@@ -33,7 +33,6 @@ thanatos_dark_fill_with_name <- generate_scale_fill_with_name(thanatos_dark)
 #' @include theme_generator.R
 thanatos_dark_color_discrete <- generate_scale_color_discrete("thanatos-dark",
                                                               thanatos_dark)
-
 #' @export
 #' @include theme_generator.R
 thanatos_dark_fill_discrete <- generate_scale_fill_discrete("thanatos-dark",
